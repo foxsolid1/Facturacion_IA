c0a3ff83d13b11cb278488c9179a72cd595e2036
foxsolid1 <foxsolid22df@gmail.com>
2025-11-26 22:44:26 -0600

    Add config.js with demo mode for GitHub Pages on main branch

---
+++ config.js
@@ -0,0 +1,14 @@
+﻿const CONFIG = {
+    SPREADSHEET_ID: '1VqGwm7KBxWbULlNvmDPbkO8VdPqLTqVYx_5rGqLqLqQ',
+    GOOGLE_CLIENT_ID: 'tu-client-id.apps.googleusercontent.com',
+    APP_NAME: 'FacturaControl - DEMO',
+    VERSION: '1.0.0',
+    CACHE_DURATION: 5 * 60 * 1000,
+    IS_DEMO: true,
+};
+function getSheetURL() {
+    return `https://docs.google.com/spreadsheets/d/c:\Users\foxso\OneDrive - Universidad Abierta y a Distancia de México\Documentos\Facturacion con IA{CONFIG.SPREADSHEET_ID}/gviz/tq?tqx=out:json`;
+}
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = CONFIG;
+}
